56dedf3c5922fa7ba8e126024c4a4a2e2edac971
LogicalJoe <47825483+LogicalJoe@users.noreply.github.com>
2019-03-24 15:36:34 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,25 @@
 /*
  *--------------------------------------
  * Program Name: Off
- * Author: LogicalJoe, who else?
+ * Author: LogicalJoe
  * Copyright: Copyright (C) 2019 LogicalJoe
  * License: LJL General Public License
- * Description: None
+ * Description:
  *--------------------------------------
  */
+
 /* Keep these headers */
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <tice.h>
 
-/* Standard headers (recommended) */
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <graphx.h>
-//#include <keypadc.h>
-//#include <fileioc.h>
-
-/* Put all your globals here */
-
+#define LCD_POWER (1 << 11)
 
 void main(void) {
     /* Fill in the body of the main function here */
-    gfx_Begin(gfx_8bpp);
-    gfx_ZeroScreen();
-    gfx_SetTextTransparentColor(0);
-    gfx_SetTextFGColor(255);
-    gfx_SetTextBGColor(0);
-    gfx_PrintStringXY("LCD OFF",135,116);
+    uint8_t A=lcd_BacklightLevel;
+    lcd_BacklightLevel=0xFF;
+    lcd_Control &= ~LCD_POWER;
     delay(10000);
-    while (!os_GetCSC());
-    gfx_End();
+    while (54!=os_GetCSC());
+    lcd_BacklightLevel=A;
+    lcd_Control |= LCD_POWER;
 }
